f547b17db229a387079e88cee5ab53e0be7ca174
Stijn Aa <stijn.aa@gmail.com>
2019-06-14 23:10:12 +0200

    written some code

--- server.js
+++ server.js
@@ -60,9 +60,7 @@ const getTopic = {
   },
   dwdd: async function () {
     let dwdd = [];
-    const browser = await puppeteer.launch({
-      headless: false
-  });
+    const browser = await puppeteer.launch();
     let selector = '[class="button expand lazyload"]';
 
     const page = await browser.newPage();
@@ -105,9 +103,7 @@ const getTopic = {
   jinek: async function(){
 
   let jinek = [];
-    const browser = await puppeteer.launch({
-      headless: false
-  });
+    const browser = await puppeteer.launch();
     let selector = '[class="scroll-top"]';
 
     const page = await browser.newPage();
