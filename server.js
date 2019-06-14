4e93773f1fa2d8a805fc6084a8a452577712a3ec
Stijn Aa <stijn.aa@gmail.com>
2019-06-14 23:05:25 +0200

    written some code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
 const rp = require('request-promise');
+const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const bodyParser = require('body-parser');
 
@@ -17,6 +18,8 @@ const reqproces = dialogflow({
 });
 
 const pauwUrl = 'https://pauw.bnnvara.nl/gemist/fragment/datum/altijd/pagina/';
+const dwddUrl = "https://dewerelddraaitdoor.bnnvara.nl/gemist/fragment/datum/altijd"
+const jinekUrl  = "https://evajinek.kro-ncrv.nl/uitzendingen/programma/jinek"
 let list = [];
 
 
@@ -49,13 +52,101 @@ const getTopic = {
           //handle error
         });
     }
-    host = {
+    show = {
       "pauw": pauw
     }
-    list.push(host)
+    list.push(show)
     //console.log(list)
-  }
+  },
+  dwdd: async function () {
+    let dwdd = [];
+    const browser = await puppeteer.launch({
+      headless: false
+  });
+    let selector = '[class="button expand lazyload"]';
+
+    const page = await browser.newPage();
+
+    await page.goto(dwddUrl, {waitUntil: 'networkidle2'});
+    await page.waitFor(selector)
+    await page.focus(selector)
+    for (i = 0; i < 5; i++) {
+      await page.click(selector)
+      await page.waitFor(1000);
+    }
+    await page.evaluate(() => document.body.innerHTML)
+
+      .then(function (html) {
+        const length = $('.item-footer', html).length;
+        console.log(length)
+         for (let i = 0; i < length; i++) {
+          const date = $('[pubdate="pubdate"]', html)[i].children[0].data.replace(/(\r\n|\n|\r)/gm, "").trim()
+          const topic = $('.item-footer > h3 > a', html)[i].children[0].data.replace(/(\r\n|\n|\r)/gm, "").trim()
+          console.log(topic)
+          const newDate = date.substr(date.indexOf(" ") + 1)
+          obj = {}
+          obj.date = new Date(setDate(newDate))
+          console.log(new Date(setDate(newDate)))
+          obj.topic = topic
+          if (!dwdd.includes(obj)) {
+            dwdd.push(obj); 
+          }
+         }
+      })
+
+
+    show = {
+      "dwdd": dwdd
+    }
+    list.push(show)
+    await browser.close();
+    console.log("Iam done!");
+  },
+  jinek: async function(){
+
+  let jinek = [];
+    const browser = await puppeteer.launch({
+      headless: false
+  });
+    let selector = '[class="scroll-top"]';
+
+    const page = await browser.newPage();
+
+    await page.goto(jinekUrl, {waitUntil: 'networkidle2'});
+    for (i = 0; i < 5; i++) {
+      await page.$eval(selector, (el) => el.scrollIntoView())
+      await page.waitFor(1000);
+    }
+    await page.evaluate(() => document.body.innerHTML)
+
+      .then(function (html) {
+        const length = $('.js-full-click-enabled', html).length;
+        console.log(length)
+         for (let i = 0; i < length - 5 ; i++) {
+          const date = $('[class="date-display-single"]', html)[i].children[0].data.replace(/(\r\n|\n|\r)/gm, "").trim()
+          const topic = $('[class="ds-1col node node-fragment view-mode-tile "] > h2 > a', html)[i].children[0].data.replace(/(\r\n|\n|\r)/gm, "").trim()
+          console.log(topic)
+          obj = {}
+          obj.date = new Date(setDate(date))
+          console.log(new Date(setDate(date)))
+          obj.topic = topic
+          if (!jinek.includes(obj)) {
+            jinek.push(obj); 
+          }
+         }
+      })
+
+
+    show = {
+      "jinek": jinek
+    }
+    list.push(show)
+    await browser.close();
+    console.log("Iam done!");
+  },
+
 }
+
 
 let month = new Map()
 
@@ -75,17 +166,17 @@ function setDate(date) {
   return newDate
 }
 
-function selector(_host, reqdate) {
+function selector(_show, reqdate) {
   let topics = []
   //console.log(_host)
-  list.forEach(host => {
+  list.forEach(show => {
     // console.log(_host, Object.keys(host).toString())
-    if (Object.keys(host).toString() === _host) {
+    if (Object.keys(show).toString() === _show) {
       //console.log(Object.values(host)[0])
-      Object.values(host)[0].forEach(element => {
+      Object.values(show)[0].forEach(element => {
         //console.log(element.date.toISOString().split("T")[0], reqdate.toISOString().split("T")[0])
         if (element.date.toISOString().split("T")[0] === reqdate.toISOString().split("T")[0]) {
-            topics.push(element.topic)
+          topics.push(element.topic)
           console.log("push")
         }
       })
@@ -121,6 +212,9 @@ reqproces.intent('GetTopic', (conv, params) => {
 app.post('/webhook', reqproces);
 
 getTopic.pauw();
+getTopic.dwdd();
+getTopic.jinek()
+
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
